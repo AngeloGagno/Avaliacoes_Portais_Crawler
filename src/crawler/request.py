3e9685e5849d5ad25459c90afb852929d7220808
AngeloGagno <angelogagno@gmail.com>
2025-05-22 18:34:17 -0300

    feat:requisição para obter os dados

---
+++ src/crawler/request.py
@@ -0,0 +1,20 @@
+import requests
+from datetime import datetime,timedelta
+
+class Portal_Reviews:
+
+    def __init__(self,cookies:dict):
+        self.session_cookies = cookies
+    
+    def request(self):
+        request = requests.get(self.url(), cookies=self.session_cookies)
+        try:
+            return request.json()
+        except:
+            raise ConnectionError
+        
+    def url(self):
+        first_date = datetime.now() - timedelta(days=5)
+        end_date = datetime.now()
+        return 'https://app.pineapples.com.br/channelmanager-api/v1/' \
+    'accommodations?channel=bk_airbnb&limit=1000'
